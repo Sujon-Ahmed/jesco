25db8f742005ab326fafd19c41aa570cc263cca5
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-06-29 11:37:40 +0600

    product show in front page

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
@@ -10,8 +11,10 @@ class FrontendController extends Controller
     public function index()
     {
         $categories = Category::where('status', '1')->get();
+        $products = Product::all();
         return view('frontend.index', [
             'categories' => $categories,
+            'products' => $products,
         ]);
     }
 }
