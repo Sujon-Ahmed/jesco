e15bef703e2df14ecbcdb15eed8253636c83b12d
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-06-30 17:12:06 +0600

    latest product & category view in frontend page

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,10 +11,15 @@ class FrontendController extends Controller
     public function index()
     {
         $categories = Category::where('status', '1')->get();
+        $latest_categories = Category::latest()->take(4)->get();
+        $latest_product = Product::latest()->get();
         $products = Product::all();
+        
         return view('frontend.index', [
             'categories' => $categories,
+            'latest_categories' => $latest_categories,
             'products' => $products,
+            'latest_product' => $latest_product,
         ]);
     }
 }
